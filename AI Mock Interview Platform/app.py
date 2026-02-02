2407146df68eab290312217f61c0a4ccfcbb4da3
Abhinav Kumar <abhinavkumar3375@gmail.com>
2026-02-02 22:53:37 +0530

    Update app.py

--- AI Mock Interview Platform/app.py
+++ AI Mock Interview Platform/app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# ------------------ MODELS data  ------------------hello 
+# ------------------ MODELS data  ------------------hello world 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
