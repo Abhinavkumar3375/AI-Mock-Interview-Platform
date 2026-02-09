0fdf8e32cf7826693f437658282631c5a3e9dd39
Abhinav Kumar <abhinavkumar3375@gmail.com>
2026-02-09 20:26:22 +0530

    Update app.py

--- AI Mock Interview Platform/app.py
+++ AI Mock Interview Platform/app.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# ------------------ MODELS ------------------
+# ------------------ MODELS -
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -574,3 +574,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
