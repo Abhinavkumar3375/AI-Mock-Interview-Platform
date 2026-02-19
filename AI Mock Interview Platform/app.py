28fc810a89cf9f17f19c9d2c4f744f5155d61aee
Abhinav Kumar <abhinavkumar3375@gmail.com>
2026-02-19 23:41:52 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- AI Mock Interview Platform/app.py
+++ AI Mock Interview Platform/app.py
@@ -8,8 +8,7 @@ app.secret_key = "secure_mock_ai_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# hello every one---------\
-
+# hello 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
@@ -574,6 +573,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
 
 
 
